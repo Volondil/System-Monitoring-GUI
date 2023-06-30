d18a85f94d0c3cdfb84a07fec3894363c07d9f7e
Volondil <yulrmath@gmail.com>
2023-06-30 02:46:39 +0200

    Initial commit

--- amd.py
+++ amd.py
@@ -14,24 +14,4 @@
  #
  # You should have received a copy of the GNU General Public License
  # along with nvtop.  If not, see <http://www.gnu.org/licenses/>.
-import pyamdgpuinfo as gpuInfo
-import psutil
-
-class infos:
-    def __init__(self, gpu):
-        #gpu = gpuInfo.get_gpu(gpu_id)
-        self.name = gpu.name
-        self.load = int(gpu.query_load() * 100)
-        self.memoryUsage = round(gpu.query_vram_usage() / 1073741824, 1)
-        self.clock = int(round(gpu.query_sclk() / 1000000, 2))
-        self.power = gpu.query_power()
-        self.voltage = round(gpu.query_graphics_voltage(), 3)
-        self.fansMode = get_gpu_fans_mode(hwmon_path + '/pwm1_enable')
-        self.maxClocks = int(gpu.query_max_clocks()['sclk_max'] / 1000000)
-        self.maxMemory = round(gpu.memory_info['vram_size'] / 1073741824, 1)
-        self.maxMemoryClock = int(gpu.query_max_clocks()['mclk_max'] / 1000000)
-        self.edgeTemp = int(psutil.sensors_temperatures()["amdgpu"][0][1])
-        self.junctionTemp = int(psutil.sensors_temperatures()["amdgpu"][1][1])
-        self.fansSpeed = psutil.sensors_fans()["amdgpu"][0][1]
-        self.memoryTemp = int(psutil.sensors_temperatures()["amdgpu"][2][1])
 
--- nvidia.py
+++ nvidia.py
@@ -14,27 +14,3 @@
  #
  # You should have received a copy of the GNU General Public License
  # along with nvtop.  If not, see <http://www.gnu.org/licenses/>.
-import GPUtil as gpu_info
-
-def get_nvidia_infos(gpus):
-    list_gpus = []
-    for gpu in gpus:
-        gpu_id = gpu.id
-        gpu_name = gpu.name
-        gpu_load = gpu.load*100
-        gpu_f_memory = gpu.memoryFree
-        gpu_u_memory = gpu.memoryUsed
-        gpu_t_memory = gpu.memoryTotal
-        gpu_temp = gpu.temperature
-        gpu_uuid = gpu.uuid
-        list_gpus.append((
-            gpu_id,
-            gpu_name,
-            gpu_load,
-            gpu_f_memory,
-            gpu_u_memory,
-            gpu_t_memory,
-            gpu_temp,
-            gpu_uuid
-        ))
-    return list_gpus
